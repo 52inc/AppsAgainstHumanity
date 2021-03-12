5dc82692035f28cbf7b43422a62316f4e68df90d
Drew Heavner <veedubusc@gmail.com>
2021-03-12 10:29:01 -0500

    Fixed most of the issues and got the app to run in the web

--- lib/internal/dynamic_links.dart
+++ lib/internal/dynamic_links.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 
@@ -5,54 +6,60 @@ class DynamicLinks {
     DynamicLinks._();
 
     static Future<Uri> createLink(String gameDocumentId) async {
-        final DynamicLinkParameters params = DynamicLinkParameters(
-            uriPrefix: "https://appsagainsthumanity.com/games",
-            link: Uri.parse("https://appsagainsthumanity.com/games/$gameDocumentId"),
-            androidParameters: AndroidParameters(
-                packageName: 'com.ftinc.appsagainsthumanity',
-            ),
-            iosParameters: IosParameters(
-                appStoreId: "1509268296",
-                bundleId: "com.ftinc.appsagainsthumanity",
-            ),
-            socialMetaTagParameters: SocialMetaTagParameters(
-                title: "Come join my game!",
-                description: "Play a game of Apps Against Humanity",
-            ),
-        );
+        if (!kIsWeb) {
+            final DynamicLinkParameters params = DynamicLinkParameters(
+                uriPrefix: "https://appsagainsthumanity.com/games",
+                link: Uri.parse("https://appsagainsthumanity.com/games/$gameDocumentId"),
+                androidParameters: AndroidParameters(
+                    packageName: 'com.ftinc.appsagainsthumanity',
+                ),
+                iosParameters: IosParameters(
+                    appStoreId: "1509268296",
+                    bundleId: "com.ftinc.appsagainsthumanity",
+                ),
+                socialMetaTagParameters: SocialMetaTagParameters(
+                    title: "Come join my game!",
+                    description: "Play a game of Apps Against Humanity",
+                ),
+            );
 
-        var shortLink = await params.buildShortLink();
-        return shortLink.shortUrl;
+            var shortLink = await params.buildShortLink();
+            return shortLink.shortUrl;
+        } else {
+            return Uri.parse("https://appsagainsthumanity.com/games/$gameDocumentId");
+        }
     }
 
     static void initDynamicLinks(BuildContext context, void Function(String gameId) callback) async {
-        final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.getInitialLink();
-        final Uri deepLink = data?.link;
+        if (!kIsWeb) {
+            final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.getInitialLink();
+            final Uri deepLink = data?.link;
 
-        if (deepLink != null) {
-            var lastSegment = deepLink.pathSegments.last;
-            if (lastSegment != null) {
-                print("Joining $lastSegment from dynamic link");
-                callback(lastSegment);
-            }
-        }
-
-        FirebaseDynamicLinks.instance.onLink(
-            onSuccess: (PendingDynamicLinkData dynamicLink) async {
-                final Uri deepLink = dynamicLink?.link;
-
-                if (deepLink != null) {
-                    var lastSegment = deepLink.pathSegments.last;
-                    if (lastSegment != null) {
-                        print("Joining $lastSegment from dynamic link");
-                        callback(lastSegment);
-                    }
+            if (deepLink != null) {
+                var lastSegment = deepLink.pathSegments.last;
+                if (lastSegment != null) {
+                    print("Joining $lastSegment from dynamic link");
+                    callback(lastSegment);
                 }
-            },
-            onError: (OnLinkErrorException e) async {
-                print('onLinkError');
-                print(e.message);
             }
-        );
+
+            FirebaseDynamicLinks.instance.onLink(
+                onSuccess: (PendingDynamicLinkData dynamicLink) async {
+                    final Uri deepLink = dynamicLink?.link;
+
+                    if (deepLink != null) {
+                        var lastSegment = deepLink.pathSegments.last;
+                        if (lastSegment != null) {
+                            print("Joining $lastSegment from dynamic link");
+                            callback(lastSegment);
+                        }
+                    }
+                },
+                onError: (OnLinkErrorException e) async {
+                    print('onLinkError');
+                    print(e.message);
+                }
+            );
+        }
     }
 }
